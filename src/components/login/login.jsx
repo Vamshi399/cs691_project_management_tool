a0218a563b1f8ceb47dbcc3a239c90bc6b1b1143
vamshi399 <vamshi399@gmail.com>
2020-10-19 18:47:50 -0400

    committing styles for login/signup

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -53,11 +53,11 @@ export class Login extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" value={this.state.username} onChange={this.handleInputChange} />
+              <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleInputChange} />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleInputChange}/>
+              <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange}/>
             </div>
           </div>
         </div>
